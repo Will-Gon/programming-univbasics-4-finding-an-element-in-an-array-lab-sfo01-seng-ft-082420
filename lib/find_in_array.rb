0803c3e952e35b75c780fe855776160ddd8bbc36
Will Gonzalez <will.colocho@gmail.com>
2020-08-07 05:46:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  while count < value_to_find do 
-    puts value_to_find
+  while count < array.length do 
+    puts array[count]
     count += 1 
   end
 end
